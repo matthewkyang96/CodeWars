133519b68bfa3abee441c81b8d587e5e0f32d118
Matt Y <49535190+matthewkyang96@users.noreply.github.com>
2024-03-02 12:43:29 -0800

    Commit distint.js challenge

---
+++ distinct.js
@@ -0,0 +1,25 @@
+/* Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+
+Examples:
+
+Input -> Output
+[1, 1, 2] -> [1, 2]
+[1, 2, 1, 1, 3, 2] -> [1, 2, 3] 
+
+https://www.codewars.com/kata/57a5b0dfcf1fa526bb000118/train/javascript
+*/
+
+//Solution
+function distinct(a) {
+    let seen = [];
+    return a.filter(num =>{
+        if(seen.includes(num)){
+            return false
+        } else {
+            seen.push(num);
+            return true
+        }
+    })
+}
