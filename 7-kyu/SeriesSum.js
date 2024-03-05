1bad36cbb4c5bc973cbe921e1fc1298d0037b0cc
Matt Y <49535190+matthewkyang96@users.noreply.github.com>
2024-03-05 12:52:54 -0800

    Commit SeriesSum.js challenge

---
+++ 7-kyu/SeriesSum.js
@@ -0,0 +1,27 @@
+/* Task:
+Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+Rules:
+You need to round the answer to 2 decimal places and return it as String.
+
+If the given value is 0 then it should return 0.00
+
+You will only be given Natural Numbers as arguments.
+
+Examples:(Input --> Output)
+1 --> 1 --> "1.00"
+2 --> 1 + 1/4 --> "1.25"
+5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+https://www.codewars.com/kata/555eded1ad94b00403000071
+*/
+
+//Solution
+function SeriesSum(n) {
+    let sum = 0;
+    for(let i = 0; i < n; i++) {
+      sum += 1 / (3 * i + 1);
+    }
+    return sum.toFixed(2);
+  }
