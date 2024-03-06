f8e9679024d590a126f2bccf44f906f0e38758b6
Matt Y <49535190+matthewkyang96@users.noreply.github.com>
2024-03-06 14:16:35 -0800

    Commit unusualFive.js challenge

---
+++ unusualFive.js
@@ -0,0 +1,12 @@
+/* Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/ 
+
+
+https://www.codewars.com/kata/59441520102eaa25260000bf/javascript
+*/
+
+//Solution
+function unusualFive() {
+    return "FIVE!".length
+}
