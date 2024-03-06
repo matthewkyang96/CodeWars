5df7ad07860b3ff3196593603dde9b97d4d43826
Matt Y <49535190+matthewkyang96@users.noreply.github.com>
2024-03-06 14:18:47 -0800

    Commit digPow.js challenge

---
+++ 6-kyu/digPow.js
@@ -0,0 +1,35 @@
+/* Some numbers have funny properties. For example:
+
+89 --> 8¹ + 9² = 89 * 1
+695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+Given two positive integers n and p, we want to find a positive integer k, if it exists, such that the sum of the digits of n raised to consecutive powers starting from p is equal to k * n.
+
+If it is the case we will return k, if not return -1.
+
+Note: n and p will always be strictly positive integers.
+
+Examples:
+n = 89; p = 1 ---> 1 since 8¹ + 9² = 89 = 89 * 1
+
+n = 92; p = 1 ---> -1 since there is no k such that 9¹ + 2² equals 92 * k
+
+n = 695; p = 2 ---> 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
+
+n = 46288; p = 3 ---> 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51 
+
+https://www.codewars.com/kata/5552101f47fc5178b1000050
+*/
+
+//Solution
+function digPow(n, p){
+    let digitArr = n.toString().split('').map(Number);
+    let sum = digitArr.reduce((accum,curr)=>{
+      accum += Math.pow(curr,p);
+      p++;
+      return accum;
+    },0)
+    
+    return sum % n == 0 ? sum/n : -1
+  
+}
