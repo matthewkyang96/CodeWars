a870a270ee50fba82eb89fb2c5c63f3b7c786edd
Matt Y <49535190+matthewkyang96@users.noreply.github.com>
2024-03-13 13:45:46 -0700

    Refactor maxMultiple.js challenge for O(1) time complexity

--- 7-kyu/maxMultiple.js
+++ 7-kyu/maxMultiple.js
@@ -28,9 +28,5 @@ https://www.codewars.com/kata/5aba780a6a176b029800041c
 
 //Solution
 function maxMultiple(divisor, bound){
-    for(let i = bound; bound > 0; i--){
-      if(i % divisor == 0){
-        return i;
-      }
-    }
-  }
+    return bound - bound % divisor
+}
