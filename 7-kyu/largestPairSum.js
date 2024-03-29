e16a0273ba384344d86c15b33230202bf01d087f
Matt Y <49535190+matthewkyang96@users.noreply.github.com>
2024-03-29 10:17:34 -0700

    commit largestPairSum.js challenge

---
+++ 7-kyu/largestPairSum.js
@@ -0,0 +1,15 @@
+/* Given a sequence of numbers, find the largest pair sum in the sequence.
+
+For example
+
+[10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+[99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
+Input sequence contains minimum two elements and every element is an integer.
+
+https://www.codewars.com/kata/556196a6091a7e7f58000018/train/javascript */
+
+//Solution
+function largestPairSum (numbers) {
+    numbers.sort((a,b) => b - a);
+    return numbers[0] + numbers[1]
+  }
