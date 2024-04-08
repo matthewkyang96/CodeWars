f75fe629f3d63aac2e62002663f1ed993c832cd2
Matt Y <49535190+matthewkyang96@users.noreply.github.com>
2024-04-08 16:06:57 -0700

    commit matrixAddition.js challenge

---
+++ 6-kyu/matrixAddition.js
@@ -0,0 +1,42 @@
+/* Write a function that accepts two square matrices (N x N two dimensional arrays), and return the sum of the two. Both matrices being passed into the function will be of size N x N (square), containing only integers.
+
+How to sum two matrices:
+
+Take each cell [n][m] from the first matrix, and add it with the same [n][m] cell from the second matrix. This will be cell [n][m] of the solution matrix. (Except for C where solution matrix will be a 1d pseudo-multidimensional array).
+
+Visualization:
+
+|1 2 3|     |2 2 1|     |1+2 2+2 3+1|     |3 4 4|
+|3 2 1|  +  |3 2 3|  =  |3+3 2+2 1+3|  =  |6 4 4|
+|1 1 1|     |1 1 3|     |1+1 1+1 1+3|     |2 2 4|
+Example
+matrixAddition(
+  [ [1, 2, 3],
+    [3, 2, 1],
+    [1, 1, 1] ],
+//      +
+  [ [2, 2, 1],
+    [3, 2, 3],
+    [1, 1, 3] ] )
+
+// returns:
+  [ [3, 4, 4],
+    [6, 4, 4],
+    [2, 2, 4] ]
+
+https://www.codewars.com/kata/526233aefd4764272800036f
+ */
+
+
+//Solution
+function matrixAddition(a, b) {
+  let res = [];
+  for (let n = 0; n < a.length; ++n) {
+    res.push([]);
+    for (let m = 0; m < a.length; ++m) {
+      res[n][m] = a[n][m] + b[n][m];
+    }
+  }
+
+  return res;
+}
