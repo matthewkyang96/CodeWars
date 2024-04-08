e14a6976221da6195ee2dcca1b71cb4beedc763a
Matt Y <49535190+matthewkyang96@users.noreply.github.com>
2024-04-08 16:09:23 -0700

    commit fizzbuzz.js challenge

---
+++ 7-kyu/fizzbuzz.js
@@ -0,0 +1,15 @@
+function fizzbuzz(n) {
+  let res = [];
+  for (let i = 1; i <= n; ++i) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      res.push("FizzBuzz");
+    } else if (i % 3 == 0) {
+      res.push("Fizz");
+    } else if (i % 5 == 0) {
+      res.push("Buzz");
+    } else {
+      res.push(i);
+    }
+  }
+  return res;
+}
