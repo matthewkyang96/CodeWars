3b29aa0c687cd5eb488f094d4a38336052a47461
Matt Y <49535190+matthewkyang96@users.noreply.github.com>
2024-04-12 21:34:53 -0700

    commit adjacentElementsProduct.js

---
+++ 7-kyu/adjacentElementsProduct.js
@@ -0,0 +1,28 @@
+/* Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
+
+Notes
+Array/list size is at least 2.
+
+Array/list numbers could be a mixture of positives, negatives also zeroes .
+
+Input >> Output Examples
+adjacentElementsProduct([1, 2, 3]); ==> return 6
+Explanation:
+The maximum product obtained from multiplying 2 * 3 = 6, and they're adjacent numbers in the array.
+adjacentElementsProduct([9, 5, 10, 2, 24, -1, -48]); ==> return 50
+Explanation:
+Max product obtained from multiplying 5 * 10  =  50 .
+
+adjacentElementsProduct([-23, 4, -5, 99, -27, 329, -2, 7, -921])  ==>  return -14
+Explanation:
+The maximum product obtained from multiplying -2 * 7 = -14, and they're adjacent numbers in the array.
+ */
+
+//Solution
+function adjacentElementsProduct(array) {
+  let max = -Infinity;
+  for (let i = 0; i < array.length - 1; ++i) {
+    max = Math.max(array[i] * array[i + 1], max);
+  }
+  return max;
+}
