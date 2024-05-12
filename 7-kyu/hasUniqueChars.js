f4feb9b7bf3e3705f8bc43c7ccc185bc1add7754
Matt Y <49535190+matthewkyang96@users.noreply.github.com>
2024-05-12 14:59:57 -0700

    commit hasUniqueChars.js challenge

---
+++ 7-kyu/hasUniqueChars.js
@@ -0,0 +1,8 @@
+// Write a program to determine if a string contains only unique characters. Return true if it does and false otherwise.
+
+// The string may contain any of the 128 ASCII characters. Characters are case-sensitive, e.g. 'a' and 'A' are considered different characters.
+
+function hasUniqueChars(str){
+    const set = new Set(str.split(''));
+    return set.size == str.length
+  }
