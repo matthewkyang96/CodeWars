7d63662e0996682942650f60006e627b908a88c2
Matt Y <49535190+matthewkyang96@users.noreply.github.com>
2024-07-23 20:53:06 -0700

    refactor nthFibo.js challenge

--- 6-kyu/nthFibo.js
+++ 6-kyu/nthFibo.js
@@ -1,10 +1,7 @@
 function nthFibo(n) {
-  if (n <= 1) {
-    return 0;
-  }
-
-  if (n == 2) {
-    return 1;
+  //base case
+  if (n <= 2) {
+    return n - 1;
   }
 
   return nthFibo(n - 1) + nthFibo(n - 2);
