15f72603fe969ea22bce059c445ed418725c1a84
Matt Y <49535190+matthewkyang96@users.noreply.github.com>
2024-07-30 21:26:17 -0700

    refactor nbrOfLaps.js challenge to use recursion for greatest common divisor function.

--- 6-kyu/nbrOfLaps.js
+++ 6-kyu/nbrOfLaps.js
@@ -1,14 +1,12 @@
 var nbrOfLaps = function (x, y) {
-  const gcdValue = gcd(x, y);
-  const lcm = (x * y) / gcdValue;
+  let lcm = (x * y) / gcd(x, y);
+
   return [lcm / x, lcm / y];
 };
 
-function gcd(a, b) {// 4 ,6
-  while (b !== 0) {
-    let temp = b; // temp = 6 // temp = 4 // temp = 2
-    b = a % b; // b = 4 // b = 2 // b = 0
-    a = temp; // a = 6 // a = 4 // a = 2
+function gcd(a, b) {// (4,6)
+  if (b == 0) {
+    return a;
   }
-  return a;
+  return gcd(b, a % b); // (6, 4) // (4, 2) // (2, 0)
 }
