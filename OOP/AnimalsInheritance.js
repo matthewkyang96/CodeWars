f2f8f10ecc5f8bda949ce20ff5bebdc721e1f6b3
Matt Y <49535190+matthewkyang96@users.noreply.github.com>
2024-07-31 20:34:00 -0700

    commit animals and inheritance challenge

---
+++ OOP/AnimalsInheritance.js
@@ -0,0 +1,30 @@
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, "shark", status);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, "cat", status);
+  }
+
+  introduce() {
+    return super.introduce() + "  Meow meow!";
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, "dog", status);
+    this.master = master;
+  }
+
+  introduce() {
+    return super.introduce();
+  }
+
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
+}
