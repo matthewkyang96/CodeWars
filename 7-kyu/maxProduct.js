be0e17bec80ca02be9287b229aa0933cab51819f
Matt Y <49535190+matthewkyang96@users.noreply.github.com>
2024-08-08 21:13:32 -0700

    refactor maxProduct.js the javascripty way

--- 7-kyu/maxProduct.js
+++ 7-kyu/maxProduct.js
@@ -1,5 +1,6 @@
 function maxProduct(numbers, size) {
-  numbers.sort((a, b) => b - a);
-  let subArr = numbers.slice(0, size);
-  return subArr.reduce((a, c) => a * c, 1);
+  return numbers
+    .sort((a, b) => b - a)
+    .slice(0, size)
+    .reduce((a, b) => a * b);
 }
