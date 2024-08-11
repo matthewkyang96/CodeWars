4ae09084860056a66a92741a5d48cb9832143181
Matt Y <49535190+matthewkyang96@users.noreply.github.com>
2024-08-10 17:33:25 -0700

    refactor Journey.js challenge ES6 syntax

--- OOP/Journey.js
+++ OOP/Journey.js
@@ -1,12 +1,13 @@
-function Journey(object, crew, balloons) {
-  this.weight = object.weight;
-  this.crew = crew;
-  this.balloons = balloons;
+class Journey {
+  constructor(object, crew, balloons) {
+    this.weight = object.weight;
+    this.crew = crew;
+    this.balloons = balloons;
+  }
 
-  this.isPossible = function () {
-    let totalWeight = this.weight + this.crew * 80;
+  isPossible() {
+    let totWeight = this.weight + this.crew * 80;
     let balloonCap = this.balloons * 4.8 * 0.001;
-
-    return balloonCap > totalWeight;
-  };
+    return balloonCap > totWeight;
+  }
 }
