47429f34e1fdeac823bf7d82587ffbf2c84c5a22
Matt Y <49535190+matthewkyang96@users.noreply.github.com>
2024-08-19 18:02:58 -0700

    commit personFactory.js challenge

---
+++ OOP/personFactory.js
@@ -0,0 +1,25 @@
+function person(
+  firstName,
+  lastName,
+  age,
+  gender,
+  employed,
+  occupation,
+  married
+) {
+  return {
+    firstName: firstName,
+    lastName: lastName,
+    age: age,
+    gender: gender,
+    employed: employed,
+    occupation: occupation,
+    married: married,
+    sayName: function () {
+      return this.firstName + " " + this.lastName;
+    },
+    introduce: function () {
+      return `Hello, my name is ${this.firstName} ${this.lastName}.  I am ${this.age} years old.  I am a ${this.gender}.`;
+    },
+  };
+}
