20fda6a8d476df15f9114b6d1e29190eb273f09b
Matt Y <49535190+matthewkyang96@users.noreply.github.com>
2024-09-18 21:54:35 -0700

    refactor reverse_words.c again

--- c/reverse_words.c
+++ c/reverse_words.c
@@ -1,30 +1,29 @@
-#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
 
-char *reverse_words (const char *words, size_t length, char *reversed)
+char *reverse_words(const char *words, size_t length, char *reversed)
 {
-  //initialize reversed array with empty spaces
-  memset(reversed, ' ', length);
-  
-  char* pointer_space; // space pointer
-  
-  //initialize pointer 1 address beyond the end of the length
-  char* pointer_reversedstr = &reversed[length + 1];
-   
-  do{
-    pointer_space = strchr(words, ' '); //point to the address of the first occurence of ' ' character
-    int word_len;
-    if(pointer_space){
-      word_len = pointer_space - words; // pointer arithmetic to find word length
-    } else {
-      word_len = strlen(words);
+    reversed[length] = '\0';
+    char *r = reversed;
+    size_t end_index = length - 1;
+    size_t start_index = length;
+
+    while (start_index != 0)
+    {
+        start_index--;
+        if (words[start_index] == ' ')
+        {
+            strncpy(r, words + start_index + 1, end_index - start_index);
+            r += end_index - start_index;
+            *r++ = ' ';
+            end_index = start_index - 1;
+        }
+
+        if (start_index == 0)
+        {
+            strncpy(r, words, end_index + 1);
+        }
     }
-    pointer_reversedstr -= word_len + 1;
-    memcpy(pointer_reversedstr, words, word_len);
-    words += word_len + 1;
 
-  } while(pointer_space);
-  
-  reversed[length] = '\0';
-
-	return reversed;
+    return reversed;
 }
