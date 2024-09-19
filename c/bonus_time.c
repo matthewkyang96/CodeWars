49b7d8f728482f8eb22a172b6cbfc885aaa69d67
Matt Y <49535190+matthewkyang96@users.noreply.github.com>
2024-09-18 21:57:51 -0700

    refactor bonus_time.c

--- c/bonus_time.c
+++ c/bonus_time.c
@@ -1,18 +1,13 @@
 #include <stdbool.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 typedef unsigned long long ull;
 
 char *bonus_time(ull salary, bool bonus)
 {
-    char *result = malloc(sizeof(char) * 1000);
-    result[0] = '\0';
-    if (bonus)
-    {
-        salary *= 10;
-    }
-    sprintf(result, "$%llu", salary);
+    char *money;
 
-    return result;
+    asprintf(&money, "$%llu", bonus ? salary * 10 : salary);
+
+    return money;
 }
